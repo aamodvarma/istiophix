826c30bb9b82b4566687927602f86e80a957f16c
Aamod Varma <aamodvarma@gmail.com>
2021-12-30 15:12:25 +0400

    change

--- static/js/nav.js
+++ static/js/nav.js
@@ -1,11 +1,1 @@
-       /* Set the width of the side navigation to 250px */
-       function openNav() {
-           document.getElementById("mySidenav").style.width = "250px";
-           document.getElementById("navmain").style.display = 'none';
-       }
-
-       /* Set the width of the side navigation to 0 */
-       function closeNav() {
-           document.getElementById("mySidenav").style.width = "0";
-           document.getElementById("navmain").style.display = 'flex';
-       }
+}
